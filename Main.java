f372d89a9f8d424fa6f6130faa40ea4ee5ca715f
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-03-23 01:58:13 +0200

    Main and readme update

---
+++ Main.java
@@ -0,0 +1,154 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        String[] symbols = { "🔔", "🍒", "🍋", "⭐️", "🍉" };
+
+        int bet;
+        int balance = 100;
+        boolean play = true;
+        int bellCount = 0;
+        int starCount = 0;
+        int cherryCount = 0;
+        int lemonCount = 0;
+        int watermelonCount = 0;
+        String decision;
+
+        draw_stars(1);
+        System.out.println("    Welcome to Java Slots");
+        System.out.printf("Symbols: %s%n", Arrays.toString(symbols));
+        draw_stars(1);
+
+        while (play) {
+
+            if (balance == 0) {
+                System.out.println("Insufficient funds, cannot continue\nGOODBYE!");
+                play = false;
+            }
+            System.out.printf("Current balance: %d\n", balance);
+            System.out.print("Place your bet amount: ");
+            bet = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Spinning...");
+
+            balance -= bet;
+
+            String[] randomSymbols = new String[3];
+
+            for (int i = 0; i < randomSymbols.length; i++) {
+                randomSymbols[i] = symbols[random.nextInt(symbols.length)];
+            }
+            System.out.println(Arrays.toString(randomSymbols));
+
+            for (String symbol : randomSymbols) {
+                switch (symbol) {
+                    case "🔔" -> {
+                        bellCount += 1;
+                        switch (bellCount) {
+                            case 3 -> bet *= 8;
+                            case 2 -> bet *= 5;
+                        }
+                    }
+                    case "⭐️" -> {
+                        starCount += 1;
+                        switch (starCount) {
+                            case 3 -> bet *= 9;
+                            case 2 -> bet *= 6;
+
+                        }
+                    }
+                    case "🍒" -> {
+                        cherryCount += 1;
+                        switch (cherryCount) {
+                            case 3 -> bet *= 5;
+                            case 2 -> bet *= 3;
+
+                        }
+                    }
+                    case "🍋" -> {
+                        lemonCount += 1;
+                        switch (lemonCount) {
+
+                            case 3 -> bet *= 5;
+                            case 2 -> bet *= 4;
+                        }
+                    }
+                    case "🍉" -> {
+                        watermelonCount += 1;
+                        switch (watermelonCount) {
+                            case 3 -> bet *= 6;
+                            case 2 -> bet *= 4;
+                        }
+                    }
+                }
+            }
+
+            int[] symbolCounts = { bellCount, starCount, watermelonCount, cherryCount, lemonCount };
+            int lossScore = 0;
+
+            for (int symbolCount : symbolCounts) {
+                if (symbolCount >= 2) {
+                    System.out.printf("You won $%d\n", bet);
+                    balance += bet;
+                    break;
+                } else {
+                    if (symbolCount == 0) {
+                        continue;
+                    } else {
+                        lossScore += 1;
+                    }
+                }
+            }
+            if (lossScore == 3) {
+                System.out.println("Sorry you lost this round");
+            }
+
+            System.out.println("Do you want to play again? (Y/N): ");
+            decision = scanner.next().toLowerCase();
+
+            switch (decision) {
+                case "y" -> {
+                    bet = 0;
+                    bellCount = 0;
+                    starCount = 0;
+                    watermelonCount = 0;
+                    cherryCount = 0;
+                    lemonCount = 0;
+
+                    System.out.println();
+                }
+                case "n" -> {
+                    System.out.printf("GAME OVER! Your final balance is %d", balance);
+                    play = false;
+                }
+                default -> {
+                    System.out.println("Invalid Entry! Game Over");
+                    play = false;
+                }
+            }
+
+        }
+
+        scanner.close();
+
+    }
+
+    static void draw_stars(int number) {
+
+        int amount = (number == 1) ? 30 : 8;
+        for (int i = 0; i < amount; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # slot-machine
+
 "A simple Java slot machine game with betting and randomized spins."
+
+okay so, this was an interesting project
+This project formed part of me completing the Bro Code Java Course:
+https://youtu.be/xTtL8E4LzTQ?si=SRlXrK3x7muo11a9
+
+okay so, i completed this project while only viewing his output, so it may not be exactly the same
+nonethless, i'm not particularly a fan of his teaching style...not gonna dive too deep
+
+uhm, this project has no error handling in terms of a user entering the wrong input at certain points
+that's because i haven't learned a try and catch code yet
+
+overall, awesome project had fun building it
